6daf9fa34f8b8417c283e830007ddc840b8deeed
Angelo Pengue <apengue@vigilantglobal.com>
2018-12-22 11:36:05 -0500

    Corrects client build directory in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}../client/build/index.html`));
+  res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
 });
 
 module.exports = app;
