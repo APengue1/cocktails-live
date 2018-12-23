6e69452efe759928391c016d958bdacd537e1980
Angelo Pengue <apengue@vigilantglobal.com>
2018-12-22 21:52:03 -0500

    Adds state to Orders

--- src/server/models/order.js
+++ src/server/models/order.js
@@ -1,7 +1,24 @@
 class Order {
+  static get QUEUED()           { return "queued" }
+  static get PROGRESS()      { return "in_progress" }
+  static get READY() { return "ready_for_pickup" }
+
   constructor(consumer_identity, cocktail) {
     this.consumer_identity = consumer_identity
     this.cocktail = cocktail
+    this.state_queued()
+  }
+
+  state_queued() {
+    this.state = Order.QUEUED
+  }
+
+  state_progress() {
+    this.state = Order.PROGRESS
+  }
+
+  state_ready() {
+    this.state = Order.READY
   }
 }
 
--- src/server/spec/models/order_spec.js
+++ src/server/spec/models/order_spec.js
@@ -1,26 +1,51 @@
 const Order = require.main.require('./models/order')
 
 describe("Order", () => {
+  beforeEach(() => {
+    this.consumer_identity = "consumer_identity"
+    this.cocktail = {
+      "name": "AmarettoSour",
+      "ingredients": [
+        {
+          "name": "Amaretto"
+        },
+        {
+          "name": "Lemon"
+        },
+        {
+          "name": "Ice"
+        }
+      ]
+    }
+    this.subject = new Order(this.consumer_identity, this.cocktail)
+  })
+
   describe("#constructor", () => {
     it("returns the customer identity", () => {
-      const consumer_identity = 'consumer_identity'
-      const cocktail = {
-        "name": "AmarettoSour",
-        "ingredients": [
-          {
-            "name": "Amaretto"
-          },
-          {
-            "name": "Lemon"
-          },
-          {
-            "name": "Ice"
-          }
-        ]
-      }
-      const subject = new Order(consumer_identity, cocktail)
-      expect(subject.consumer_identity).toEqual(consumer_identity)
-      expect(subject.cocktail).toEqual(cocktail)
+      expect(this.subject.consumer_identity).toEqual(this.consumer_identity)
+      expect(this.subject.cocktail).toEqual(this.cocktail)
+      expect(this.subject.state).toEqual(Order.QUEUED)
+    })
+  })
+
+  describe("#state_queued", () => {
+    it("puts the order in queued state", () => {
+      this.subject.state_queued()
+      expect(this.subject.state).toEqual(Order.QUEUED)
+    })
+  })
+
+  describe("#state_progress", () => {
+    it("puts the order in progress state", () => {
+      this.subject.state_progress()
+      expect(this.subject.state).toEqual(Order.PROGRESS)
+    })
+  })
+
+  describe("#state_ready", () => {
+    it("puts the order in ready state", () => {
+      this.subject.state_ready()
+      expect(this.subject.state).toEqual(Order.READY)
     })
   })
 })
