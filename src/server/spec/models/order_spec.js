88896446f15ce747565eb62d5fdb73baf211d169
Angelo Pengue <apengue@vigilantglobal.com>
2018-12-22 21:16:10 -0500

    Corrects order_spec to send json

--- src/server/spec/models/order_spec.js
+++ src/server/spec/models/order_spec.js
@@ -1,13 +1,26 @@
 const Order = require.main.require('./models/order')
-const ABC = require.main.require('./models/cocktails/abc')
 
 describe("Order", () => {
   describe("#constructor", () => {
     it("returns the customer identity", () => {
-      const abc = new ABC()
-      const subject = new Order("consumer_identity", abc)
-      expect(subject.consumer_identity).toEqual("consumer_identity")
-      expect(subject.cocktail).toEqual(abc)
+      const consumer_identity = 'consumer_identity'
+      const cocktail = {
+        "name": "AmarettoSour",
+        "ingredients": [
+          {
+            "name": "Amaretto"
+          },
+          {
+            "name": "Lemon"
+          },
+          {
+            "name": "Ice"
+          }
+        ]
+      }
+      const subject = new Order(consumer_identity, cocktail)
+      expect(subject.consumer_identity).toEqual(consumer_identity)
+      expect(subject.cocktail).toEqual(cocktail)
     })
   })
 })
