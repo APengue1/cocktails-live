fb108f022af7d5d4c57314849a0a7d9430a1254d
Angelo Pengue <apengue@vigilantglobal.com>
2018-12-23 14:59:00 -0500

    Adds OrderService state change methods

--- src/server/services/order_service.js
+++ src/server/services/order_service.js
@@ -1,12 +1,24 @@
-const Queue = require.main.require('./lib/queue')
-
 class OrderService {
   constructor() {
-    this.order_queue = new Queue()
+    this.orders = []
   }
 
   enqueue_order(order) {
-    this.order_queue.enqueue(order)
+    order.state_queued()
+    this.orders.push(order)
+  }
+
+  progress_order(order) {
+    this.find_order(order).state_progress()
+  }
+
+  ready_order(order) {
+    this.find_order(order).state_ready()
+  }
+
+  find_order(order_to_find) {
+    const found_order = this.orders.find(order => order.equals(order_to_find))
+    return found_order ? found_order : order_to_find
   }
 }
 
--- src/server/spec/services/order_service_spec.js
+++ src/server/spec/services/order_service_spec.js
@@ -22,13 +22,53 @@ describe("OrderService", () => {
     this.subject = new OrderService()
   })
 
+  describe("#find_order", () => {
+    beforeEach(() => {
+      this.subject.enqueue_order(this.order)
+    })
+    describe("when the order is found", () => {
+      it("returns the found order", () => {
+        const order = this.subject.find_order(this.order)
+        expect(order).toEqual(this.order)
+      })
+    })
+    describe("when the order is not found", () => {
+      it("returns the order to find", () => {
+        const order = this.subject.find_order({})
+        expect(order).toEqual({})
+      })
+    })
+  })
+
   describe("#enqueue_order", () => {
     beforeEach(() => {
       this.subject.enqueue_order(this.order)
     })
-  
     it("adds the order to the queue", () => {
-      expect(this.subject.order_queue.dequeue()).toEqual(this.order)
+      expect(this.subject.orders.shift()).toEqual(this.order)
+    })
+    it("puts the order in queued state", () => {
+      expect(this.order.state).toEqual(Order.QUEUED)
+    })
+  })
+
+  describe("#progress_order", () => {
+    beforeEach(() => {
+      this.subject.enqueue_order(this.order)
+      this.subject.progress_order(this.order)
+    })
+    it("puts the order in progress state", () => {
+      expect(this.order.state).toEqual(Order.PROGRESS)
+    })
+  })
+
+  describe("#ready_order", () => {
+    beforeEach(() => {
+      this.subject.enqueue_order(this.order)
+      this.subject.ready_order(this.order)
+    })
+    it("puts the order in ready state", () => {
+      expect(this.order.state).toEqual(Order.READY)
     })
   })
 })
