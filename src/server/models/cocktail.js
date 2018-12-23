fb2634cc851bdc27b554c82e4f64b5e3bca330fe
Angelo Pengue <apengue@vigilantglobal.com>
2018-12-22 19:48:31 -0500

    Added json() method to cocktails

--- src/server/models/cocktail.js
+++ src/server/models/cocktail.js
@@ -9,6 +9,12 @@ class Cocktail extends Consumable {
   consumable_files_pattern() {
     return 'cocktails/*.js'
   }
+
+  json() {
+    return Object.assign(super.json(), {
+      ingredients: this.ingredients.map(ingredient => ingredient.json())
+    })
+  }
 }
 
 module.exports = Cocktail
