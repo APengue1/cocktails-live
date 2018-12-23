339d3c897971ca33d8971f044bfa799fe8281ded
Angelo Pengue <apengue@vigilantglobal.com>
2018-12-22 19:48:17 -0500

    Added json() method to consumables

--- src/server/models/consumable.js
+++ src/server/models/consumable.js
@@ -8,14 +8,18 @@ class Consumable {
   all_json() {
     return this.consumable_files().map(consumable_file => {
       const consumable = new (require(consumable_file))()
-      return {
-        name: consumable.name()
-      }
+      return consumable.json()
     })
   }
 
   consumable_files() {
     return glob(__dirname, this.consumable_files_pattern())
+  }
+
+  json() {
+    return {
+      name: this.name()
+    }
   }
 }
 
