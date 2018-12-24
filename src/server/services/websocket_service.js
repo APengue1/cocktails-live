78a92668e64d967f6c2201b1e533ed75751707b5
Angelo Pengue <apengue@vigilantglobal.com>
2018-12-23 20:00:57 -0500

    WebsocketService handles ws close

--- src/server/services/websocket_service.js
+++ src/server/services/websocket_service.js
@@ -5,11 +5,19 @@ class WebsocketService {
 
   add_websocket(websocket) {
     this.websockets.push(websocket)
+    websocket.on('close', () => this.on_websocket_close(websocket))
   }
 
   send_message(message) {
     for(const ws of this.websockets) {
       ws.send(message)
+    }
+  }
+
+  on_websocket_close(websocket) {
+    const websocket_index = this.websockets.indexOf(websocket)
+    if(websocket_index >= 0) {
+      this.websockets.splice(websocket_index, 1)
     }
   }
 }
--- src/server/spec/services/websocket_service_spec.js
+++ src/server/spec/services/websocket_service_spec.js
@@ -3,13 +3,26 @@ const WebsocketService = require.main.require('./services/websocket_service')
 describe("WebsocketService", () => {
   beforeEach(() => {
     this.subject = new WebsocketService()
-    this.ws = jasmine.createSpyObj('ws', ['send']);
+    this.ws = jasmine.createSpyObj('ws', ['send', 'on']);
   })
 
   describe("#add_websocket", () => {
+    beforeEach(() => this.subject.add_websocket(this.ws))
     it("adds the websocket to its list of websockets", () => {
-      this.subject.add_websocket(this.ws)
       expect(this.subject.websockets.shift()).toEqual(this.ws)
+    })
+    it("registers the on_close handler", () => {
+      expect(this.ws.on).toHaveBeenCalledWith('close', jasmine.any(Function))
+    })
+  })
+
+  describe("on_websocket_close", () => {
+    beforeEach(() => {
+      this.subject.add_websocket(this.ws)
+      this.subject.on_websocket_close(this.ws)
+    })
+    it("removes the websocket from its list of websockets", () => {
+      expect(this.subject.websockets.length).toEqual(0)
     })
   })
 
@@ -20,7 +33,7 @@ describe("WebsocketService", () => {
     it("sends the given message to all websockets", () => {
       const message = { message: "message" }
       this.subject.send_message(message)
-      expect(this.ws.send).toHaveBeenCalledWith(message);
+      expect(this.ws.send).toHaveBeenCalledWith(message)
     })
   })
 })
